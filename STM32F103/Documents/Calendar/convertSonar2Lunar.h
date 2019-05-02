6b72faaa57c66b082b2a7ead0fb77ce60188d9e2
hoaitv <hoaitv@bkav.com>
2019-05-02 15:26:13 +0700

    update convert solar to lunar calendar

--- STM32F103/Documents/Calendar/convertSonar2Lunar.h
+++ STM32F103/Documents/Calendar/convertSonar2Lunar.h
@@ -9,13 +9,12 @@
 #define CONVERTSONAR2LUNAR_H_
 
 //2 bien luu gia tri ngay va thang am lich
-long int thangAl,ngayAl;
-long int jdFromDate(unsigned char dd,unsigned char mm,unsigned int yy);
-void jdToDate(long int jd);
-float getNewMoonDay(long int k,unsigned char timeZone);
-float getSunLongitude(float jdn,unsigned char timeZone);
-long int getLunarMonth11(unsigned int yy,unsigned char timeZone);
-unsigned int getLeapMonthOffset(long int a11,unsigned char timeZone);
-void convertSolar2Lunar(unsigned char dd,unsigned char mm,unsigned int yy,unsigned char timeZone);
-
+int thangAl,ngayAl,namAl;
+long getJulius(int intNgay, int intThang, int intNam);
+int getNewMoonDay(int k);
+int getSunLongitude(int jdn);
+int getLunarMonthll(int intNam);
+int getLeapMonthOffset(double a11);
+int convertSolar2Lunar(int intNgay, int intThang, int intNam);
+int main() ;
 #endif /* CONVERTSONAR2LUNAR_H_ */
